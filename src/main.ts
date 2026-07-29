b2214e8d1473f1bddd5e33a76f44aa05f29a1ca9
Ashzuu <evan.dynak@bbox.fr>
2026-07-29 11:35:28 +0200

    fix: changing the global handler in main to put only trace & message

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,4 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err) => console.error("Une erreur est survenue : " + err.message + "\n" + err.stack));
